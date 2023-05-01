f4d7fbf2b91884817c132f3f1599497f28324b68
侯志凯 <hou17513213314@163.com>
2023-05-01 17:44:44 +0800

    init

---
+++ src/renderer/App.tsx
@@ -0,0 +1,17 @@
+// import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+// import icon from '../../assets/icon.svg';
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+
+// function Hello() {
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
+
+function AppPage() {
+  return <Button type="primary">12221111</Button>;
+}
+export default AppPage;
